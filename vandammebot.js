93dd97c1d884b64e7caebc8677f5ca413a8dfd6d
Anna-Livia <annalivia.cardin@gmail.com>
2017-03-06 17:05:12 +0100

    fills the read me with a to do and roadmap

---
+++ vandammebot.js
@@ -0,0 +1,25 @@
+
+var API_Key = "d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de" ;
+var login =  "XXX" ;
+var mdp = "XXX" ;
+
+var trigger = ;
+var reponse_vandamme = ;
+
+#toutes les 10 min :
+
+	#Je me connecte
+	# je lis les nouveau messages
+	# si il y en a des nouveau :
+		#j analyse les messages
+		# si le message contiient une trigger
+			# je répond
+
+	return {
+	"title": "string",
+	"topic_id": 0,
+	"raw": "string",
+	"category": 0,
+	"target_usernames": "discourse1,discourse2",
+	"archetype": "private_message"
+	}
