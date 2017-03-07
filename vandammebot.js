d97f6846ae4217ba2927e602a6879f206cdea58b
Lorena Rangel <rangel.loreen@gmail.com>
2017-03-07 17:19:39 +0100

    "things i chqnged in the morning (I think)

--- vandammebot.js
+++ vandammebot.js
@@ -6,13 +6,16 @@
 
 /**Curl works in console with 
 "http://communaute.amontourdeprogrammer.fr?api_key=d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de&api_username=yannick"
-it is important that it has Yannick's username to get the content, or else nothing will show. We must be missing something here
+It seems it could be any user that has access o 
 **/
 
 var request = require('request');
-request('http://communaute.amontourdeprogrammer.fr/t/premiere-semaine-faire-un-bot-en-javascript/27/22?api_key=d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de&api_username=yannick', function (error, response, body) {
+
+var a = request('http://communaute.amontourdeprogrammer.fr/t/titres-et-certifications-que-les-apprenantes-peuvent-passer/16.json?api_key=d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de&api_username=yannick', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
   console.log('body:', body); // Print the HTML for the Google homepage. 
 }) //ça marche!! 
 
+
+//console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOH", a);
