3f17616e8641bd98494ddfbf3fb65148c457736a
Lisa Durand <lisa@superdudu.net>
2017-03-10 12:07:38 +0100

    vérification qu'un topic est nouveau,s'il est nouveau on post 1msg sur ce topic

--- vandammebot.js
+++ vandammebot.js
@@ -1,4 +1,5 @@
 
+var got = require('got');
 // This function tells me if a message is new or not
 // Input : message object
 // Output : true if new, False if not new
@@ -10,7 +11,7 @@ function isItNew(post) {
 	if (post.read == false) {
 			//keep in the list
 			return true;
-		} else { 
+		} else {
 			//discard
     			console.log('NOOOOOOOOOPE !!!!!!!');
     		return false;
@@ -22,42 +23,46 @@ function isItNew(post) {
 // Input : URL of discourse topic + api_key + api_username
 // Output : list of post on the topic (as js objects)
 
-var urltopic = 'http://communaute.amontourdeprogrammer.fr/t/comptes-github-et-twitter-de-chacunes/40.json?api_key=d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de&api_username=Anna-Livia'
+var urltopic = 'http://communaute.amontourdeprogrammer.fr/t/test-a-ne-pas-consulter-svp/72/1.json?api_key=d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de&api_username=Anna-Livia'
 //var urltopic = 'http://communaute.amontourdeprogrammer.fr/t/cle-publique-et-chiffrement-rsa/50.json?api_key=d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de&api_username=LoreenRH'
 
+function filterAndRespond(response) {       // get the list of posts
+    listOfPosts = JSON.parse(response.body).post_stream.posts;
+    // filter the list with the "is it new function
+    console.log("list of", listOfPosts.length, typeof(listOfPosts));
+    var filteredPosts = listOfPosts.filter(isItNew);
+    console.log("after filter");
+
+    var postUrl = 'http://communaute.amontourdeprogrammer.fr/posts';
+
+
+    got.post(postUrl, {body: {
+      "api_key": "d4fb34eff83af60a16791e0807c39c2dfbfcc8a81005536dd9c432fe6ca8f6de",
+      "api_username": "Anna-Livia",
+      "topic_id": 72,
+      "raw": "Ceci est du faux texte de plus de 20 char pour tester depuis vandame 2",
+    }
+    })
+
+.catch(error => {
+    console.log(error.response.body);
+    //=> 'Internal server error ...'
+});
+
+}
+
 function getposts(url) {
-	const fs = require('fs');
-	const got = require('got');
 
 	var listOfPosts = [] ;
 	console.log('start getposts')
 	// Open a url
-	got(url)
-	    .then(response => {
-	    	// get the list of posts
-	    	console.log("before list of posts");
-	        listOfPosts = JSON.parse(response.body).post_stream.posts;
-	        console.log("after list of posts");
-	        // filter the list with the "is it new function
-	        console.log("list of", listOfPosts.length, typeof(listOfPosts));
-	        var filteredPosts = listOfPosts.filter(isItNew);
-	        console.log("after filter");
-			console.log(filteredPosts);
-	        //=> '<!doctype html> ...' 
-	    })
+	got.get(url)
+	    .then(filterAndRespond)
 	    .catch(error => {
 	        console.log(error.response.body);
-	        //=> 'Internal server error ...' 
+	        //=> 'Internal server error ...'
 	    });
 	console.log("end of getposts");
 	}
 
 getposts(urltopic);
-//http://docs.discourse.org/
-
-//var listOfPosts = ;
-
-
-
-
-//console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOH", a);
